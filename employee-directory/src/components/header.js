6283358b500baac417e9fffe26e5629b810f2f66
kikaiaku <tjscarb89@aol.com>
2020-09-08 18:11:47 -0400

    fixed issues with read me and css

--- employee-directory/src/components/header.js
+++ employee-directory/src/components/header.js
@@ -6,7 +6,7 @@ function Header () {
     return (
       <div className="header">
         <h1>Employee Directory</h1>
-        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        <p>Click on the search bar to look up a specific person.</p>
       </div>
     )
   }
